1440153828d1a1409580ebe781356bc8e411c098
wwq <wangweiquan333@163.com>
2016-08-04 23:13:27 +0800

    first commit

---
+++ src/main/java/com/wwq/frame/service/userService/impl/PersonServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wwq.frame.service.userService.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wwq.frame.domain.Person;
+import com.wwq.frame.mapper.PersonMapper;
+import com.wwq.frame.service.userService.PersonService;
+
+public class PersonServiceImpl implements PersonService {
+
+	
+	@Autowired
+	private PersonMapper personMapper;
+
+	public Person getPersonById(long id) throws Exception {
+		return personMapper.getPersonById(id);
+	}
+
+	
+	
+}
